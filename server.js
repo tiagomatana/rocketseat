bcd8d055c13e7c07d7462fe45715a4367c7afa24
Tiago Matana <tiago.matana@gmail.com>
2018-11-01 18:09:07 -0300

    Pronto Rest em NodeJs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
+app.use(express.json());
 
 //conexao com o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true});
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 //ROUTE
-routes.get('/products', ProductController.index);
+routes.get('/products', ProductController.list);
+routes.get('/products/:id', ProductController.find);
+routes.post('/products', ProductController.create);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.delete);
 
 module.exports = routes;
